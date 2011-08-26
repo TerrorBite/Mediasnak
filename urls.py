705982489f39ed65a3cc361242a78da7a9e8edc5
Ben <addres@gmail.com>
2011-08-27 03:19:01 +1000

    made a development-files directory to store extra production files edited the favicon, added it to the url.py list made an icon for the site

--- urls.py
+++ urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls.defaults import *
 
+# On the deployed version, homedir will always be ''
+import os
+homedir = os.path.abspath(os.path.dirname(__file__))
+
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
@@ -9,4 +13,6 @@ urlpatterns = patterns('',
      
     ('^upload$', 'views.upload_form',
      {}),
+    ('^favicon.ico$', 'django.views.static.serve',
+     {'path': 'favicon.ico', 'document_root' : homedir}),
 )
