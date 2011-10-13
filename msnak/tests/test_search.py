535416a5182f5142aa91e7fa5e5625ff0ca7ba51
unknown <n7555555@.qut.edu.au>
2011-10-13 10:30:07 +1000

    Added tests for searching
    2x tests for searching 'everything' *
    2x tests for searching 'filename'
    
    * - everything currently only searches 'filename' and 'fileid'

--- msnak/tests/test_search.py
+++ msnak/tests/test_search.py
@@ -2,26 +2,45 @@ from django.utils.unittest import TestCase # This is django's copy of the unitte
 from django.test import TestCase # Has stuff like assertContains, would like to use, but is giving errors when included
 from unittest import TestCase
 from msnak.models import MediaFile # Import our database model
+import msnak.listfiles
 
 class TestSearch(TestCase):
+    #assertTrue()
+    #assertEquals()
 
     def setUp(self):
-        MediaFile(file_id="u/tests/file1", user_id=0, filename="testfile1.png", upload_time="2011-01-01 00:00", view_count=100).save()
-        MediaFile(file_id="u/tests/file2", user_id=0, filename="duplicatefilename.png", upload_time="2011-01-01 00:00", view_count=100).save()
-        MediaFile(file_id="u/tests/file3", user_id=0, filename="duplicatefilename.png", upload_time="2011-01-01 00:00", view_count=100).save()
-        MediaFile(file_id="u/tests/pQv-O2sFKi8", user_id=0, filename="Jellyfish.jpg", upload_time="2011-01-01 00:00", view_count=100).save()
+        MediaFile(file_id="u/tests/file1", user_id=0, filename="test.png", upload_time="2011-01-01 00:00", view_count=100).save()
+        MediaFile(file_id="u/tests/file2", user_id=0, filename="duplicatefilename.png", upload_time="2011-01-01 00:00", view_count=7).save()
+        MediaFile(file_id="u/tests/file3", user_id=0, filename="duplicatefilename.png", upload_time="2011-01-01 00:00", view_count=20).save()
+        MediaFile(file_id="u/tests/random", user_id=0, filename="somefile1.jpg", upload_time="2011-01-01 00:00", view_count=90).save()
+        MediaFile(file_id="u/tests/pQv-O2sFKi8", user_id=0, filename="Jellyfish.jpg", upload_time="2011-01-01 00:00", view_count=350).save()
 
     #couple of searches searching the database for everything
     def searchEverything1(self):
-        #self.assertTrue()
-        pass
-
+        results = search_files("s3.mediasnak.com",0,"default","file")
+        count = 0
+        for item in results:
+            count = count + 1
+        self.assertEquals(count, 4)
+        
     def searchEverything2(self):
-        pass
+        results = search_files("s3.mediasnak.com",0,"default","1") ##NOTE: NOT SEARCHING UPLOAD TIME OR VIEW COUNT YET
+        count = 0
+        for item in results:
+            count = count + 1
+        self.assertEquals(count,2)
 
     #couple of searches searching the database for filenames
     def searchFileName1(self):
-        pass
+        results = search_files("s3.mediasnak.com",0,"default",".jpg")
+        count = 0
+        for item in results:
+            count = count + 1
+        self.assertEquals(count,2)
 
     def searchFileName2(self):
-        pass
+        results = search_files("s3.mediasnak.com",0,"default","file")
+        count = 0
+        for item in results:
+            count = count + 1
+        self.assertEquals(count,3)
